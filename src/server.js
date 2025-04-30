073b63c5db780fddfeb002f8863bc41afeaf99de
devops4sure <ramchandrasriharsha@gmail.com>
2025-04-30 12:33:22 +0530

    Changes in server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://4fe3d11a.greekgodsquad-website.pages.dev/",
     credentials: true,
   })
 );
